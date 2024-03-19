0181c5d96e126ffeb082da0c1d79c7971c56b029
mastra90 <115628366+mastra90@users.noreply.github.com>
2024-03-19 15:18:20 +1100

    Styled the header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 import ContactFormCard from './Components/ContactFormCard';
+import Headshot from './Assets/Headshot.png'
+
 
 function App() {
   return (
-    <div> 
+    <div>
+      <header>
+        <div className='banner'></div>
+        <p className='andrew-thomas-text'>Dr Andrew Thomas</p>
+       <h3 className='intro-text'>Scholar in Middle East Studies and International Relations. Lecturer at Deakin University. Add more text here</h3>
+          <img className='headshot' src={Headshot} alt="Andrew Thomas Headshot" />
+        
+      </header> 
       <ContactFormCard />
     </div>
   );
